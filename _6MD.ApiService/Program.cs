835e89e6c34543fd9096f8b485e99822a21b509f
Chilla55 <j.k0201@gmx.de>
2025-03-06 16:00:13 +0100

    Remove Discord authentication and related controllers
    This commit removes the `DiscordController.cs` and `LogoutController.cs` files, eliminating the OAuth flow for Discord login and user logout functionality. Additionally, significant portions of the OpenIddict configuration related to Discord have been removed from `Program.cs`, meaning the application will no longer support Discord authentication. The `todo.txt` file has been updated to include a new task focused on building a single sign-on (SSO) solution.

--- _6MD.ApiService/Program.cs
+++ _6MD.ApiService/Program.cs
@@ -1,13 +1,8 @@
 using Aspire.Npgsql.EntityFrameworkCore.PostgreSQL;
 using Grpc.Core;
 using Microsoft.EntityFrameworkCore;
-using OpenIddict.EntityFrameworkCore.Models;
-using static OpenIddict.Abstractions.OpenIddictConstants;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var DiscordOauthSecret = Environment.GetEnvironmentVariable("DiscordOauthSecret") ?? throw new Exception("DiscordOauthSecret not found!");
-var DiscordOauthClientID = Environment.GetEnvironmentVariable("DiscordOauthClientID") ?? throw new Exception("DiscordOauthClientID not found!");
 
 // Add service defaults & Aspire client integrations.
 builder.AddServiceDefaults();
@@ -19,7 +14,6 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataStructure.DB>(options =>
     {
         options.UseNpgsql(builder.Configuration.GetConnectionString("MainDB"));
-        options.UseOpenIddict();
     });
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
@@ -27,62 +21,6 @@ builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 #endif
-
-builder.Services.AddOpenIddict()
-    .AddCore(options =>
-    {
-        options.UseEntityFrameworkCore()
-               .UseDbContext<DataStructure.DB>();
-    })
-    .AddServer(options =>
-    {
-        options.SetTokenEndpointUris("/connect/token")
-               .SetAuthorizationEndpointUris("/connect/authorize");
-
-        options.AllowAuthorizationCodeFlow()
-               .RequireProofKeyForCodeExchange(); // Enables PKCE
-
-        options.RegisterScopes(Scopes.Email, Scopes.Profile, Scopes.OpenId, Scopes.Roles);
-
-        // Register signing/encryption credentials
-        options.AddDevelopmentEncryptionCertificate()
-               .AddDevelopmentSigningCertificate();
-
-        // Enable ASP.NET Core host integration
-        options.UseAspNetCore()
-               .EnableAuthorizationEndpointPassthrough()
-               .EnableTokenEndpointPassthrough();
-    })
-    .AddClient(options =>
-    {
-        // Note: this sample only uses the authorization code flow,
-        // but you can enable the other flows if necessary.
-        options.AllowAuthorizationCodeFlow();
-
-        // Register the signing and encryption credentials used to protect
-        // sensitive data like the state tokens produced by OpenIddict.
-        options.AddDevelopmentEncryptionCertificate()
-               .AddDevelopmentSigningCertificate();
-
-        // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
-        options.UseAspNetCore()
-               .EnableRedirectionEndpointPassthrough();
-
-        // Register the System.Net.Http integration.
-        options.UseSystemNetHttp();
-        options.UseWebProviders()
-                .AddDiscord(options =>
-                {
-                    options.SetClientId(DiscordOauthClientID);
-                    options.SetClientSecret(DiscordOauthSecret);
-                    options.SetRedirectUri("https://localhost:5001/auth/Discord/callback");
-                });
-    })
-    .AddValidation(options =>
-    {
-        options.UseLocalServer();
-        options.UseAspNetCore();
-    });
 
 
 var app = builder.Build();
